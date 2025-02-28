a06ee8da193e22e2a3160f47a5b82c78b2d80456
VincentRanger <dakshayoffice@gmail.com>
2025-02-28 18:48:39 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Nav from './components/nav'
+import Intenrional from './components/Intenrional'
+
+function App() {
+
+  return (
+<div>
+<Nav/>
+<Intenrional/>
+</div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from '../assets/ae-logo-2022.svg'
+import a from '../assets/1.jpeg'
+import b from '../assets/2.jpg'
+import c from '../assets/3.jpeg'
+import d from '../assets/4.jpg'
+import e from '../assets/5.jpg'
+import back from '../assets/back.jpg'
+
+const Nav = () => {
+  return (
+    <div>
+        <div className='bg-cover bg-emerald-100 opacity-45 absolute top-39.5 h-105 w-full  overflow-hidden -z-10 '></div>
+    <div className='flex font-serif w-full mt-2 items-center font-medium text-emerald-900 tracking-widest'>
+        <div className='flex w-full justify-between uppercase ms-30'>
+            <a href="" className='hover:text-amber-400'>about</a>
+            <a href="" className='hover:text-amber-400'>services</a>
+            <a href="" className='hover:text-amber-400'>kind words</a>
+        </div>
+        <div className='w-full justify-items-center'><img src={logo} alt="" className='w-40' /></div>
+        <div className='flex w-full justify-between me-30  uppercase'>
+            <a href="" className='hover:text-amber-400'>gallery</a>
+            <a href="" className='hover:text-amber-400'>bubbly bar</a>
+            <a href="" className='hover:text-amber-400'>contact</a>
+        </div>
+    </div>
+
+    <div className='grid grid-cols-5 mx-30 gap-5 mt-10'>
+<div><img src={a} className='w-fit h-120' /></div>
+<div><img src={b} className='w-fit h-120'/></div>
+<div><img src={c}  className='w-fit h-120' /></div>
+<div><img src={d} className='w-fit h-120' /></div>
+<div><img src={e}  className='w-fit h-120' /></div>
+    </div>
+    </div>
+  )
+}
+
+export default Nav
